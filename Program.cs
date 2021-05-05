787a2fc44fa457735f5de96a3ac29c1b03d4f1a1
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-05-05 03:10:25 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 8. Solicite al usuario el ingreso de una frase y presente un mensaje indicando si 
+             alguna contiene la palabra "fin". */
+            string frase;
+            Console.WriteLine("Ingrese una frase");
+            frase = Console.ReadLine();
+            foreach()
+            {
+                frase.Contains("fin");
+                Console.WriteLine($"La {frase} contiene fin");
+            }
+          
+        }
+    }
+}
